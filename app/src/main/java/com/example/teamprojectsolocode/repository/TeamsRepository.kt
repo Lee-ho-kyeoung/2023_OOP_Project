b25f3205f6aa8451d20123c5c07d0b19bba2c318
lhk <lhk243@naver.com>
2023-11-22 22:19:07 +0900

    team uid 별로 다르게 띄우기

--- app/src/main/java/com/example/teamprojectsolocode/repository/TeamsRepository.kt
+++ app/src/main/java/com/example/teamprojectsolocode/repository/TeamsRepository.kt
@@ -2,6 +2,7 @@ package com.example.teamprojectsolocode.repository
 
 import androidx.lifecycle.MutableLiveData
 import com.example.teamprojectsolocode.team.Teams
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -10,7 +11,8 @@ import com.google.firebase.ktx.Firebase
 
 class TeamsRepository {
     private val database = Firebase.database
-    val teamListRef = database.getReference("teamList")
+    private val uid = Firebase.auth.uid
+    val teamListRef = database.getReference("user/${uid}/teamList")
 
     fun observeTeamList(teamlist: MutableLiveData<ArrayList<Teams>>) {
         teamListRef.addValueEventListener(object: ValueEventListener{
